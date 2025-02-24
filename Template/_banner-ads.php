35fbdb3426502f815f2e1bd622b46e380a950f2b
Nguyen Manh Dung <kiaisoft.dung.nguyen1@gmail.com>
2025-02-24 23:50:08 +0700

    init

--- Template/_banner-ads.php
+++ Template/_banner-ads.php
@@ -1,8 +1,8 @@
 <!-- Banner Ads  -->
 <section id="banner_adds">
     <div class="container py-5 text-center">
-        <img src="./assets/banner_4.webp" alt="banner1" class="img-fluid" width="430px">
-        <img src="./assets/banner_5.webp" alt="banner1" class="img-fluid" width="430px">
+        <img src="./assets/banner_1.jpg" alt="banner1" class="img-fluid" width="438px">
+        <img src="./assets/banner_2.jpg" alt="banner1" class="img-fluid" width="430px">
     </div>
 </section>
 <!-- !Banner Ads  -->
